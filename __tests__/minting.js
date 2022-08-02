b2a79c77b00641d5410749a10791663f6e159c8b
Serhii Pimenov <serhii@pimenov.com.ua>
2022-08-02 16:20:06 +0300

    added Result, minting, user transactions

---
+++ __tests__/minting.js
@@ -0,0 +1,8 @@
+import {Indexer} from "../src/index.js";
+import {DBConfig} from "./config.js";
+
+const i = new Indexer({
+    ...DBConfig
+})
+const q = await i.minting("0x310dfd70948d6b22c5e6a573719e21f0437d4fa4986a106e439a1f9d44dcae0c")
+console.log(q)
--- src/api/transactions.js
+++ src/api/transactions.js
@@ -1,11 +1,92 @@
+import {Result} from "../helpers/result.js";
+
+const userTransFields = `
+    type,
+    payload,
+    version,
+    t.hash,
+    state_root_hash,
+    event_root_hash,
+    gas_used,
+    success,
+    vm_status,
+    accumulator_root_hash,
+    t.inserted_at,
+    signature,
+    sender,
+    sequence_number,
+    max_gas_amount,
+    expiration_timestamp_secs, gas_unit_price,
+    timestamp
+`
+
 export const TransactionsAPI = {
     async transCount () {
         const sql = `
-        select * from transaction_status_count
-        union
-        select * from transaction_type_count 
-    `
-        return (await this.query(sql)).rows
+            select * from transaction_status_count
+            union
+            select * from transaction_type_count 
+        `
+        try {
+            const result = (await this.query(sql)).rows
+            return new Result(true, "OK", result)
+        } catch (e) {
+            return new Result(false, e.message, e.stack)
+        }
+    },
+
+    async minting (addr, {limit = 25, offset = 0} = {}) {
+        const sql = `
+            select ${userTransFields}
+            from transactions t
+            left join user_transactions ut on t.hash = ut.hash
+            where type = 'user_transaction'
+            and payload->>'function' like '%mint%'
+            and payload->>'arguments' like $1
+            limit $2 offset $3
+        `
+
+        try {
+            const result = (await this.query(sql, [`%${addr}%`, limit, offset])).rows
+            return new Result(true, "OK", result)
+        } catch (e) {
+            return new Result(false, e.message, e.stack)
+        }
+    },
+
+    async sentTransactions (addr, {limit = 25, offset = 0}) {
+        const sql = `
+            select ${userTransFields} 
+            from transactions t
+            left join user_transactions ut on t.hash = ut.hash
+            where type = 'user_transaction'
+            and sender = $1
+            limit $2 offset $3
+        `
+
+        try {
+            const result = (await this.query(sql, [addr, limit, offset])).rows
+            return new Result(true, "OK", result)
+        } catch (e) {
+            return new Result(false, e.message, e.stack)
+        }
+    },
+
+    async receivedTransactions (addr, {limit = 25, offset = 0}) {
+        const sql = `
+            select ${userTransFields} 
+            from transactions t
+            left join user_transactions ut on t.hash = ut.hash
+            where type = 'user_transaction'
+            and payload->>'to' = $1
+            limit $2 offset $3
+        `
+
+        try {
+            const result = (await this.query(sql, [addr, limit, offset])).rows
+            return new Result(true, "OK", result)
+        } catch (e) {
+            return new Result(false, e.message, e.stack)
+        }
     }
 }
-
