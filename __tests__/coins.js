28ed90b4ff6962aade994893538f7773dba1ba09
olton <serhii@pimenov.com.ua>
2022-08-15 17:03:57 +0300

    v0.4.0 switch from Github to npm

--- __tests__/coins.js
+++ __tests__/coins.js
@@ -16,10 +16,10 @@ let q
 q = await i.payments()
 console.log(q)
 
-q = await i.incomingPayments("0x310dfd70948d6b22c5e6a573719e21f0437d4fa4986a106e439a1f9d44dcae0c")
+q = await i.incomingPayments("0x780c1d3d602d7bfe22b2a6c27d77a8f1e6d61c23a123f857b6f64cad9318bf71")
 console.log(q)
 
-q = await i.outgoingPayments("0x310dfd70948d6b22c5e6a573719e21f0437d4fa4986a106e439a1f9d44dcae0c")
+q = await i.outgoingPayments("0x780c1d3d602d7bfe22b2a6c27d77a8f1e6d61c23a123f857b6f64cad9318bf71")
 console.log(q)
 
 
